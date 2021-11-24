62556ecc0ad7e9b87556438c69c154da10fad986
jidec <>
2021-11-24 14:53:19 -0500

    Added package installs to start, moved actual analyses out of start

--- start.R
+++ start.R
@@ -1,6 +1,18 @@
+# install packages
+install.packages("dplyr")
+install.packages("lubridate")
+install.packages("rgdal")
+install.packages("maps")
+install.packages("raster")
+install.packages("RColorBrewer")
+install.packages("lme4")
+install.packages("ape")
 
+#-----------------------
+# PREP MAIN DATAFRAMES
 #import all usa ant observations
 usa_ants <- read.csv("data/usa_ants.csv")
+View(usa_ants)
 
 #add hour and season data to dataframe
 source("src/addHourSeason.R")
@@ -10,43 +22,80 @@ View(usa_ants)
 #remove observations with no time observed
 usa_ants <- usa_ants[!(usa_ants$time_observed_at == ""),]
 
-#remove observations with 0 for hour
-#usa_ants <- usa_ants[usa_ants$hour != 0,]
+#remove non research grade observations
+usa_ants <- filter(usa_ants, quality_grade == "research")
+
+#remove ants identified only to genus, not to species
+usa_ants <- usa_ants[grepl(" ", usa_ants$scientific_name),]
+
+# remove local hours at 0, 1 or 24
+usa_ants <- filter(usa_ants, local_hour != 0)
+usa_ants <- filter(usa_ants, local_hour != 1)
+usa_ants <- filter(usa_ants, local_hour != 24)
+
+# add distance of foraging hour from 3 pm around the hottest time of the day
+usa_ants$midday_dist <- abs(usa_ants$local_hour - 15)
+
+# add coding for day/night
+usa_ants$is_night <- usa_ants$local_hour <= 6 | usa_ants$local_hour >= 21
+
+# add local hour diff column
+usa_ants$local_hour_diff <- usa_ants$local_hour - mean(usa_ants$local_hour)
+# create day df with chopped off nighttimes from both sides
+library(dplyr)
+usa_ants_day <- filter(usa_ants, local_hour > 6)
+usa_ants_day <- filter(usa_ants_day, local_hour < 20)
 
 #filter for one common species
 library(dplyr)
 campo_p <- filter(usa_ants, scientific_name == "Camponotus pennsylvanicus")
+campo_c <- filter(usa_ants, scientific_name == "Camponotus castaneus")
+campo_a <- filter(usa_ants, scientific_name == "Camponotus americanus")
+l_humile <- filter(usa_ants, scientific_name == "Linepithema humile")
+p_imparis <- filter(usa_ants, scientific_name == "Prenolepis imparis")
+t_immigrans <- filter(usa_ants, scientific_name == "Tetramorium immigrans")
 
-
-#create foraging time plots
-source("src/plotForagingTimeHists.R")
-plotForagingTimeHists(usa_ants)
-plotForagingTimeHists(usa_ants, "Camponotus pennsylvanicus", 1000)
-plotForagingTimeHists(usa_ants, "Solenopsis invicta", 800)
-
-
-# getmode function for raster to get hours of highest foraging activity in grid cells
-getmode <- function(v, ...) {
-    uniqv <- unique(v)
-    uniqv[which.max(tabulate(match(v, uniqv)))]
+#-----------------------
+# SUMMARIZE TO SPECIES & GENUS
+#summarize (condense) to species
+# quick func to get mode
+Mode <- function(x) {
+    ux <- unique(x)
+    ux[which.max(tabulate(match(x, ux)))]
 }
+# summarize to species, with meanHour, modeHour, sdHour, n (sample size), meanMD (midday_dist), propNight,
+# latCor (be careful with this, dont want to just cherry pick species)
+usa_ant_species <- usa_ants %>%
+    group_by(scientific_name) %>%
+    summarise(meanHour = mean(local_hour), modeHour = Mode(local_hour), sdHour = sd(local_hour), n = n(), meanMD = mean(midday_dist),
+              propNight = sum(is_night, na.rm = TRUE) / n(), latCor = cor(midday_dist,latitude))
+usa_ant_species$latCor <- format(usa_ant_species$latCor, scientific = FALSE)
+View(usa_ant_species)
+usa_ant_species_n100 <- filter(usa_ant_species, n >= 100)
+#plot species histograms
+hist(usa_ant_species_n100$meanHour)
+hist(usa_ant_species_n100$sdHour)
+hist(usa_ant_species_n100$meanMD)
 
-#create foraging time rasters
-source("src/plotForagingTimeRasters.R")
-plotForagingTimeRasters(usa_ants, scientificName = "Solenopsis invicta",
-                        season_num = "4", raster_res = 2, raster_function = getmode)
+# similarly summarize to genus
+usa_ant_genera <- usa_ants %>%
+    group_by(taxon_genus_name) %>%
+    summarise(meanHour = mean(local_hour), modeHour = Mode(local_hour), sdHour = sd(local_hour), n = n())
 
-#quick cor test between hour and lat
-cor.test(usa_ants$hour, usa_ants$latitude)
+usa_ant_genera_n100 <- filter(usa_ant_genera, n >= 100)
 
-#assess research grade vs non data
-rg_ants <- filter(usa_ants, quality_grade == "research")
-nrow(usa_ants)#139919 samples
-nrow(rg_ants)#67617 samples
-nrow(campo_p) #10196 samples
-nrow(filter(campo_p, quality_grade == "research")) #8866 samples
+# after start.R:
+# descriptive_foraging.R
+# lat_thermal_foraging.R
+# color_foraging.R
+# species_overlap_foraging.R
+
 
 # ideas that have come up
 # use covariance matrix to adjust foraging times given baseline iNat activity
 # given all ants, see if other ants are found in location at time
 # download baseline iNat activity and randomly sample it to create baseline
+
+# notes
+# get folks on species_overlap_foraging
+#
