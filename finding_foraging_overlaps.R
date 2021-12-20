40e5941020aa40c3cc6197a7eec59ddc7667d2e6
Neeka Sewnath <nsewnath@ufl.edu>
2021-12-20 10:29:04 -0500

    initial commit

---
+++ finding_foraging_overlaps.R
@@ -0,0 +1,14 @@
+# R Script to map species distribution polygons and quantify overlaps
+# Neeka Sewnath
+# nsewnath@ufl.edu
+
+# TODO: Import libraries
+library(raster)
+
+# TODO: Read in data
+#data <- read.csv("data/usa_ants_with_wc.csv")
+
+# TODO: Create species distribution polygons
+
+
+# TODO: Quantify overlaps
